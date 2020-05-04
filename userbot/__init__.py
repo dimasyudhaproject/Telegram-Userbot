002e63412627ebf3faaac202f5aff1001e6c34fd
Dimas Yudha Pratama <dimasyudha@e.email>
2020-05-05 05:44:52 +0700

    The ".update" command must really work rn

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -88,7 +88,7 @@ HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY", None)
 # Custom (forked) repo URL for updater.
 UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
-    "https://github.com/dimasyudhaproject/Telegram-Userbot")
+    "https://github.com/dimasyudhaproject/Telegram-Userbot.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
     "UPSTREAM_REPO_BRANCH", "beta")
 
